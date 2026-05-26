7dc03ee3190f3b261f0983e772877c3c9e6121e5
Zharov-s <s.zharov23@gmail.com>
2026-05-26 23:30:12 +0300

    Use psicho image for psychologist project

--- ENZO-STUDIO-main/src/pages/Page1.jsx
+++ ENZO-STUDIO-main/src/pages/Page1.jsx
@@ -39,7 +39,10 @@ const Page1 = () => {
       onMouseMove={mouseMoveHandler}
       className="w-full h-screen bg-white p-5 relative"
     >
-      <div className='shadow-xl shadow-gray-700 w-full h-full rounded-[50px] bg-center bg-cover bg-[url("https://static.wixstatic.com/media/f1c650_678c0ceab5194893872c60fa3be4bcdc~mv2.jpg/v1/fill/w_1865,h_913,fp_0.69_0.64,q_85,usm_0.66_1.00_0.01,enc_auto/ANZO.jpg")]'>
+      <div
+        className="shadow-xl shadow-gray-700 w-full h-full rounded-[50px] bg-center bg-cover"
+        style={{ backgroundImage: 'url("../../images/featuredWorks/psicho.jpg")' }}
+      >
         <TiltText tiltRef={tiltRef} />
         <Page1Bottom />
       </div>
